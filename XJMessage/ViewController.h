2d85c676a5f0fc60c52054c71b3f3c95f4a21098
XJIMI <fn5128@gmail.com>
2015-12-11 10:52:18 +0800

    Initial Commit

---
+++ XJMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJMessage
+//
+//  Created by XJIMI on 2015/12/11.
+//  Copyright © 2015年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XJMessage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XJMessage
+//
+//  Created by XJIMI on 2015/12/11.
+//  Copyright © 2015年 XJIMI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
